1376480778b2e76b6d48d63d6e10e2cd9389b609
Luson045 <lusonbasumatary2@gmail.com>
2025-05-12 22:32:32 +0530

    CHANGED API CODE TO TS

---
+++ src/pages/api/getstocks.ts
@@ -0,0 +1,42 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { NseIndia } from "stock-nse-india";
+const nse = new NseIndia();
+
+export const config = {
+  runtime: "nodejs",
+};
+type SuccessResponse = any; 
+type ErrorResponse = {
+  error: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<SuccessResponse | ErrorResponse>
+) {
+  const { symbol } = req.body;
+
+  if (!symbol || typeof symbol !== "string") {
+    return res.status(400).json({ error: "Missing or invalid symbol" });
+  }
+
+  try {
+    const availableSymbols = await nse.getAllStockSymbols();
+
+    const isValidSymbol = availableSymbols.some(
+      (stock) => stock.trim().toUpperCase() === symbol.trim().toUpperCase()
+    );
+
+    if (!isValidSymbol) {
+      return res
+        .status(400)
+        .json({ error: "Invalid stock symbol or it doesn't belong to NSE." });
+    }
+
+    const data = await nse.getEquityDetails(symbol);
+    return res.status(200).json(data);
+  } catch (e) {
+    console.error("API Error:", e);
+    return res.status(500).json({ error: "Failed to fetch data" });
+  }
+}
